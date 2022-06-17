02c38225b91ad486c4d219ea3a93e85320876350
Nuutti <nuutti.viitajylha@gmail.com>
2022-06-17 21:23:22 +0300

    GridHandler skeleton

---
+++ GridHandler.py
@@ -0,0 +1,12 @@
+# TODO:
+#   Implement this class
+class GridHandler:
+    # @var
+    # Class Grid
+    def __init__(self, grid):
+        self.grid = grid
+
+    # Not sure of implementation yet, array is hint for future implementation
+    # Maybe after figuring out boulder serialization etc implement this
+    def lightBoulder(self, array):
+        pass
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from Grid import Grid
 import pickle
+
+from Grid import Grid
+from GridHandler import GridHandler
 
 if __name__ == '__main__':
     grid = Grid()
     grid.lightGreen(5,5)
     grid.printGrid()
+    gridHandler = GridHandler(grid)
 
