54a046b04ee4a5c0703842b6f15f4a9337205e51
Nuutti <nuutti.viitajylha@gmail.com>
2022-06-17 22:49:09 +0300

    Cleaned main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from Grid import Grid
 from GridHandler import GridHandler
 
 if __name__ == '__main__':
-    grid = Grid()
-    grid.lightGreen(5,5)
-    grid.printGrid()
     gridHandler = GridHandler()
+    gridHandler.grid.printGrid()
 
